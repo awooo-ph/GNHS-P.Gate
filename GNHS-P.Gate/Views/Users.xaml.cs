3965ce2fdb91acfe601f876b75fa05c5a4036b16
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-01-15 21:39:25 +0800

    :heavy_plus_sign:  users

---
+++ GNHS-P.Gate/Views/Users.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GNHSP.Gate.Models;
+
+namespace GNHSP.Gate.Views
+{
+    /// <summary>
+    /// Interaction logic for Users.xaml
+    /// </summary>
+    public partial class Users : UserControl
+    {
+        public Users()
+        {
+            InitializeComponent();
+        }
+
+        private void Password_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            var usr = Password.DataContext as User;
+            if (usr == null) return;
+            if (Password.Password.Length > 0)
+                usr.Password = Password.Password;
+        }
+    }
+}
