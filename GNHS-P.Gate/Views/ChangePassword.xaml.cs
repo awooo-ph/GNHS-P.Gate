468621280e3822feb1d1436eaafe97845bfc7fb6
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-01-15 21:39:51 +0800

    :heavy_plus_sign:  change password

---
+++ GNHS-P.Gate/Views/ChangePassword.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using GNHSP.Gate.Models;
+
+namespace GNHSP.Gate.Views
+{
+    /// <summary>
+    /// Interaction logic for ChangePassword.xaml
+    /// </summary>
+    public partial class ChangePassword : UserControl
+    {
+        public ChangePassword()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (MainViewModel.Instance.CurrentUser == null) return;
+            
+            if (MainViewModel.Instance.CurrentUser.Password != CurrentPassword.Password ||
+                NewPassword.Password != NewPassword2.Password || NewPassword.Password.Length == 0)
+            {
+                InvalidPasswordMessage.Visibility = Visibility.Visible;
+                return;
+            }
+
+            InvalidPasswordMessage.Visibility = Visibility.Hidden;
+            MainViewModel.Instance.CurrentUser.Update(nameof(User.Password),NewPassword.Password);
+            CancelClicked(null,e);
+        }
+
+        private void CancelClicked(object sender, RoutedEventArgs e)
+        {
+            MainViewModel.Instance.ChangePasswordVisible = false;
+            InvalidPasswordMessage.Visibility = Visibility.Hidden;
+            CurrentPassword.Password = "";
+            NewPassword.Password = "";
+            NewPassword2.Password = "";
+        }
+    }
+}
